594d09c95f058ff4e8aeff9648143577a7b347ff
Amy Gori <amydgori@gmail.com>
2023-09-23 14:59:42 -0400

    Add sentry for error monitoring

--- project/settings.py
+++ project/settings.py
@@ -13,13 +13,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from pathlib import Path
 from corsheaders.defaults import default_headers
-
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False),
     USE_S3=(bool, False),
+    USE_SENTRY=(bool, False),
 )
 environ.Env.read_env()
 
@@ -208,3 +210,20 @@ DJOSER = {
 
 TAGGIT_CASE_INSENSITIVE = True
 APPEND_SLASH = False
+
+if env("USE_SENTRY"):
+    sentry_sdk.init(
+        dsn=env("SENTRY_DSN"),
+        integrations=[DjangoIntegration()],
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
