087aebb285211011ff7f08ac3693f585b783e533
Nathan Vogt <merrick.vogt@gmail.com>
2023-09-26 09:43:10 -0400

    changed permissions to allow anonymous users Safe Methods

--- core/custom_permissions.py
+++ core/custom_permissions.py
@@ -1,15 +1,18 @@
 from rest_framework import permissions
 
-
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return True
-        return False
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
+        # If it's a read-only method, allow access
         if request.method in permissions.SAFE_METHODS:
             return True
-        if obj.author == request.user:
+        # For other methods, only allow if user is authenticated
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        # Allow read-only methods for all
+        if request.method in permissions.SAFE_METHODS:
             return True
-        return False
+        # For other methods, only allow if user is the author
+        return obj.author == request.user
+
